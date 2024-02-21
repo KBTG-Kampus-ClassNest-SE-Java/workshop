fe68efb6d23fab7c88dc002075819a4cee1d4676
Nong AnuchitO <anuchito.code@gmail.com>
2024-02-21 10:07:22 +0700

    initiate spring api for kshop

---
+++ kshop/src/main/java/com/kampus/kshop/KshopApplication.java
@@ -0,0 +1,13 @@
+package com.kampus.kshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KshopApplication.class, args);
+	}
+
+}
---
+++ kshop/src/test/java/com/kampus/kshop/KshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kampus.kshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
