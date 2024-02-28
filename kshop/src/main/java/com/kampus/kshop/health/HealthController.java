b51ea125ae7a6ce7cacbda21579b5b0a444a6da0
Nong AnuchitO <anuchito.code@gmail.com>
2024-02-28 18:57:11 +0700

    [Nong] first draft workshop

---
+++ kshop/src/main/java/com/kampus/kshop/health/HealthController.java
@@ -0,0 +1,15 @@
+package com.kampus.kshop.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "{ \"status\": \"alive\" }";
+    }
+}
