4cdca9813fbde358957d58c86ffb908a13a61884
WeRockStar <kotchaphan.m@gmail.com>
2024-03-19 21:13:07 +0700

    add architecture test for preventing misuse annotation

--- kshop/src/test/java/com/kampus/kshop/architecture/LayeredTest.java
+++ kshop/src/test/java/com/kampus/kshop/architecture/LayeredTest.java
@@ -4,7 +4,11 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "com.kampus.kshop", importOptions = ImportOption.DoNotIncludeTests.class)
@@ -20,5 +24,22 @@ public class LayeredTest {
                     .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
                     .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
                     .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
+
+    @ArchTest
+    static final ArchRule controller_packages =
+            classes().that().areAnnotatedWith(RestController.class)
+                    .should().resideInAPackage("..controller..");
+
+    @ArchTest
+    static final ArchRule service_packages =
+            classes().that()
+                    .areAnnotatedWith(Service.class)
+                    .should().resideInAPackage("..service..");
+
+    @ArchTest
+    static final ArchRule repository_packages =
+            classes().that()
+                    .areAnnotatedWith(Repository.class)
+                    .should().resideInAPackage("..repository..");
 
 }
