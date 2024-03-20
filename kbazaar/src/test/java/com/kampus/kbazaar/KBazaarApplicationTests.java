2caeecb6174da39a1586fa75a98bbb6ed5f0e6b9
Nong AnuchitO <anuchito.code@gmail.com>
2024-03-20 10:20:06 +0700

    format file

--- kbazaar/src/test/java/com/kampus/kbazaar/KBazaarApplicationTests.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/KBazaarApplicationTests.java
@@ -1,15 +1,15 @@
 package com.kampus.kbazaar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class KBazaarApplicationTests {
 
     @Test
     void contextLoads() {
-			assertEquals(1, 1);
+        assertEquals(1, 1);
     }
 }
