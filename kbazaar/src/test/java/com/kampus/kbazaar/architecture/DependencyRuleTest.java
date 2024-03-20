ae66c410d7eb35b5caa34f5c55cb1cf9d505dde8
WeRockStar <kotchaphan.m@gmail.com>
2024-03-20 17:42:40 +0700

    add more rules for architecture testing

--- kbazaar/src/test/java/com/kampus/kbazaar/architecture/DependencyRuleTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/architecture/DependencyRuleTest.java
@@ -1,6 +1,7 @@
 package com.kampus.kbazaar.architecture;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -35,5 +36,24 @@ public class DependencyRuleTest {
                     .onlyBeAccessed()
                     .byClassesThat()
                     .areAnnotatedWith(Service.class)
+                    .allowEmptyShould(true);
+
+    @ArchTest
+    static final ArchRule repository_should_not_access_services =
+            noClasses()
+                    .that()
+                    .haveSimpleNameEndingWith("Repository")
+                    .should()
+                    .accessClassesThat()
+                    .haveSimpleNameEndingWith("Service");
+
+    @ArchTest
+    static final ArchRule service_should_not_access_controller =
+            noClasses()
+                    .that()
+                    .haveSimpleNameEndingWith("Service")
+                    .should()
+                    .accessClassesThat()
+                    .haveSimpleNameEndingWith("Controller")
                     .allowEmptyShould(true);
 }
