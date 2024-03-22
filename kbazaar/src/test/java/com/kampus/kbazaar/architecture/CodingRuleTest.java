3855241951767407748eefc5969843e3c9f14950
WeRockStar <kotchaphan.m@gmail.com>
2024-03-22 10:32:34 +0700

    allow @Value injection for architecture test

--- kbazaar/src/test/java/com/kampus/kbazaar/architecture/CodingRuleTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/architecture/CodingRuleTest.java
@@ -1,10 +1,13 @@
 package com.kampus.kbazaar.architecture;
 
-import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+import static com.tngtech.archunit.lang.conditions.ArchConditions.beAnnotatedWith;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
 
+import com.tngtech.archunit.core.domain.JavaField;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 
 @AnalyzeClasses(
@@ -12,5 +15,26 @@ import com.tngtech.archunit.lang.ArchRule;
         importOptions = ImportOption.DoNotIncludeTests.class)
 public class CodingRuleTest {
 
-    @ArchTest private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+    @ArchTest
+    private final ArchRule no_field_injection =
+            noFields()
+                    .should(annotations())
+                    .as("no classes should use field injection")
+                    .because("assumed we're preferring constructor injection");
+
+    private static ArchCondition<JavaField> annotations() {
+        ArchCondition<JavaField> annotatedWithSpringAutowired =
+                beAnnotatedWith("org.springframework.beans.factory.annotation.Autowired");
+        ArchCondition<JavaField> annotatedWithGuiceInject =
+                beAnnotatedWith("com.google.inject.Inject");
+        ArchCondition<JavaField> annotatedWithJakartaInject =
+                beAnnotatedWith("javax.inject.Inject");
+        ArchCondition<JavaField> annotatedWithJakartaResource =
+                beAnnotatedWith("javax.annotation.Resource");
+        return annotatedWithSpringAutowired
+                .or(annotatedWithGuiceInject)
+                .or(annotatedWithJakartaInject)
+                .or(annotatedWithJakartaResource)
+                .as("be annotated with an injection annotation");
+    }
 }
