0fa01d5ae5ddff7203ee4752083863c23efba586
Chinwat K <chinnawatsut@gmail.com>
2024-03-22 20:21:30 +0700

    separate unit & it test

--- kbazaar/src/test/java/com/kampus/kbazaar/KBazaarApplicationIT.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/KBazaarApplicationIT.java
@@ -2,10 +2,12 @@ package com.kampus.kbazaar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("integration-test")
 class KBazaarApplicationIT {
 
     @Test
--- kbazaar/src/test/java/com/kampus/kbazaar/product/ProductServiceTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/product/ProductServiceTest.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
