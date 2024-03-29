8a1f0e5a29e668b879f998fdabaff310e2f3a067
Nong AnuchitO <anuchito.code@gmail.com>
2024-03-29 22:13:58 +0700

    update cart entity and simplify backlog

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
@@ -1,16 +1,39 @@
 package com.kampus.kbazaar.cart;
 
-import com.kampus.kbazaar.product.Product;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import jdk.jfr.Description;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "cart")
 public class Cart {
-    private int userID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-    private Product[] products;
+    @Column(name = "username")
+    private String username;
 
-    public Cart(int userID, Product[] products) {
-        this.userID = userID;
-        this.products = products;
-    }
+    @Column(name = "discount")
+    private BigDecimal discount;
+
+    @Column(name = "total_discount")
+    private BigDecimal totalDiscount;
+
+    @Column(name = "promotion_codes")
+    private String promotionCodes;
+
+    @Description("precisely reflect its pre-discount status")
+    @Column(name = "subtotal")
+    private BigDecimal subtotal;
+
+    @Description("the final, all-inclusive amount to be paid.")
+    @Column(name = "grand_total")
+    private BigDecimal grandTotal;
 }
