89d899a72bc30a5fd4c2cd6fe5d21b162ce445ac
Chinwat K <chinnawatsut@gmail.com>
2024-03-29 21:36:13 +0700

    secuity - default response 401

--- kbazaar/src/main/java/com/kampus/kbazaar/security/SecurityConfig.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.kampus.kbazaar.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
@@ -32,6 +34,10 @@ public class SecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class)
+                .exceptionHandling(
+                        e ->
+                                e.authenticationEntryPoint(
+                                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .build();
     }
 }
